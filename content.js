f9fe9c2b630c8f9bbe764b6b30f92a7ad61ac1d6
Jason Duncan <jason@asus-laptop>
2010-12-31 14:13:11 -0500

    Cleaned up content.js (column spacing).

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
 var tabid;
 
 var callback = function(response) {
-	var scripts = response.scripts;
-	
-	// if you want to log something, send a message to dom.js which lives on the tab page
-	chrome.tabs.sendRequest(tabid, {action: "consoleLog", data: scripts}, callback);
+  var scripts = response.scripts;
+  
+  // if you want to log something, send a message to dom.js which lives on the tab page
+  chrome.tabs.sendRequest(tabid, {action: "consoleLog", data: scripts}, callback);
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-	tabid = tab.id;
-	chrome.tabs.sendRequest(tab.id, {action: "getScripts"}, callback);
+  tabid = tab.id;
+  chrome.tabs.sendRequest(tab.id, {action: "getScripts"}, callback);
 });
