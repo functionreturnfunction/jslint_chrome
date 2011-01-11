40283f6b17790244d6cbdda67b88e2d1d3e15d0f
Nicholas Ortenzio <NOrtenzio@.nba-hq.com>
2011-01-11 14:21:37 -0500

    refactored tab module added navTo method

--- src/tabs.js
+++ src/tabs.js
@@ -18,20 +18,51 @@ JSLint Extension for Google Chrome.  If not, see <http://www.gnu.org/licenses/>.
 
 (function($){
 
-  $.fn.tabs = function() {
-    return $(this).each(function(){
-      var $this = $(this).addClass('ui-tab');
-      var $navs = $this.find('> ul > li').addClass('ui-nav');
-      var $panes = $this.find('> div').addClass('ui-pane');
+  /*** Module API ***/
 
-      $navs.find('a').click(function(e){
-        e.stopPropagation();
-        var i = $navs.index($(this).parent());
-        $navs.removeClass('ui-active').eq(i).addClass('ui-active');
-        $panes.removeClass('ui-active').eq(i).addClass('ui-active');
-      }).eq(0).click();
+  var methods = {
 
-    })
+    init : function(options) { 
+
+      return $(this).each(function(){
+
+        var $this = $(this).addClass('ui-tab');
+        var $navs = $this.find('> ul > li').addClass('ui-nav');
+        var $panes = $this.find('> div').addClass('ui-pane');
+
+        $navs.find('a').click(function(e){
+          e.stopPropagation();
+          var i = $navs.index($(this).parent());
+          $navs.removeClass('ui-active').eq(i).addClass('ui-active');
+          $panes.removeClass('ui-active').eq(i).addClass('ui-active');
+        }).eq(0).click();
+
+        $this.data('tabs', {
+          $this: $this,
+          $navs: $navs,
+          $panes: $panes
+        });
+
+      })
+    },
+
+    navTo : function(selector) {
+      var data = $(this).data('tabs');
+      data.$navs.removeClass('ui-active').find('a[href="' + selector +'"]').parent().addClass('ui-active');
+      data.$panes.removeClass('ui-active').filter(selector).addClass('ui-active');
+    }
+  };
+
+  /*** Module Definition ***/
+
+  $.fn.tabs = function(method) {
+      if ( methods[method] ) {
+          return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+      } else if (typeof method === 'object' || !method) {
+          return methods.init.apply(this,arguments);
+      } else {
+          $.error('Method ' + method + ' does not exist');
+      }
   }
 
 })(jQuery);
